c1a945b59a6b697e525a12daa712be9283defbff
Husnain Ali <129890731+husnain-ali2@users.noreply.github.com>
2025-07-03 13:50:54 +0500

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,19 @@
+#Week2 task1
+#Profile information
+Name = "Husnain Ali"
+age = 24
+current_year = 2025
+country = "Pakistan"
+hobby = "social anlysis"
+expected_graduation_year = 2025+1
+
+years_left = expected_graduation_year - current_year
+
+print("My Profile Information")
+print(f"Name: {Name}")
+print(f"Age: {age}")
+print(f"Current Year: {current_year}")
+print(f"Country: {country}")
+print(f"Hobby: {hobby}")
+print(f"Expected Graduation Year: {expected_graduation_year}")
+print(f"Years left for graduation: {years_left}")
